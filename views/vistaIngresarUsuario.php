5138ce0ab0ed519340b8c59cbac950b40e5d477a
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-11-26 18:29:38 -0500

    Modifcacion de la vista para ingresar datos

--- views/vistaIngresarUsuario.php
+++ views/vistaIngresarUsuario.php
@@ -26,7 +26,7 @@ function mostrarFormularioIngreso($mensaje){
 <?php
 }
 ?>
-<link rel="stylesheet" href="<?php echo get_UrlBase('css/style-ingresardatos.css'); ?>"> 
+<link rel="stylesheet" href="<?php echo get_UrlBase('css/style-ingresarUsuario.css'); ?>"> 
 <link rel="stylesheet" href="<?php echo get_UrlBase('css/style-mensaje-emergente.css'); ?>">
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script> 
 <script src="<?php echo get_UrlBase('js/modelo-mensaje-emergente.js'); ?>"></script>
