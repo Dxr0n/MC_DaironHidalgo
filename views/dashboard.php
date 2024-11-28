326d36a40db1e6e86078af9496bb0be4f4eda8a3
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-11-28 17:38:23 -0500

    Vista del Dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,5 +1,7 @@
 <?php
 function mostrarDashboard($argumento) {
+    $opcion = isset($_GET['opcion']) ? $_GET['opcion'] : '';
+    
     ?>
 
 <!DOCTYPE html>
@@ -19,16 +21,53 @@ function mostrarDashboard($argumento) {
     </div>
   
     <div class="menu">
-        <h3>Menu</h3>
-        <ul>
-            <li> <a href="?opcion=Inicio"> <img src="<?php echo get_UrlBase('img/icono-inicio.svg')?>" alt="Inicio" width="40px">Inicio</a> </li>
-            <li> <a href="?opcion=Ver"> <img src="<?php echo get_UrlBase('img/icono-ver.svg')?>" alt="Ver" width="30px">Ver</a> </li>
-            <li> <a href="?opcion=Ingresar"> <img src="<?php echo get_UrlBase('img/icono-ingresar.svg')?>" alt="Ingresar" width="30px">Ingresar</a> </li>
-            <li> <a href="?opcion=Modificar"> <img src="<?php echo get_UrlBase('img/icono-modificar.svg')?>" alt="Modificar" width="30px">Modificar</a> </li>
-            <li> <a href="?opcion=Eliminar"> <img src="<?php echo get_UrlBase('img/icono-eliminar.svg')?>" alt="Eliminar" width="30px">Eliminar</a> </li>
-            <li> <a class="salir" href="<?php echo get_controllers('logout.php')?>"> <img src="<?php echo get_UrlBase('img/icono-salir.svg')?>" alt="Salir" width="30px">Salir</a> </li>
-        </ul>
-    </div>
+    <h3>Menu</h3>
+    <ul>
+        <li>
+            <a href="?opcion=Inicio" class="<?php echo isActive('Inicio', $opcion); ?>">
+                <img class="menu-icon" 
+                     data-default-src="<?php echo get_UrlBase('img/icono-inicio.svg'); ?>" 
+                     data-alt-src="<?php echo get_UrlBase('img/icono-inicio-relleno.svg'); ?>" 
+                     src="<?php echo isActive('Inicio', $opcion) ? get_UrlBase('img/icono-inicio-relleno.svg') : get_UrlBase('img/icono-inicio.svg'); ?>" 
+                     alt="Inicio" 
+                     width="30px"> Inicio </a></li>
+        <li>
+            <a href="?opcion=Ver" class="<?php echo isActive('Ver', $opcion); ?>">
+                <img class="menu-icon" 
+                     data-default-src="<?php echo get_UrlBase('img/icono-ver.svg'); ?>" 
+                     data-alt-src="<?php echo get_UrlBase('img/icono-ver-relleno.svg'); ?>" 
+                     src="<?php echo isActive('Ver', $opcion) ? get_UrlBase('img/icono-ver-relleno.svg') : get_UrlBase('img/icono-ver.svg'); ?>" 
+                     alt="Ver" 
+                     width="30px">Ver</a></li>
+        <li>
+            <a href="?opcion=Ingresar" class="<?php echo isActive('Ingresar', $opcion); ?>">
+                <img class="menu-icon" 
+                     data-default-src="<?php echo get_UrlBase('img/icono-ingresar.svg'); ?>" 
+                     data-alt-src="<?php echo get_UrlBase('img/icono-ingresar-relleno.svg'); ?>" 
+                     src="<?php echo isActive('Ingresar', $opcion) ? get_UrlBase('img/icono-ingresar-relleno.svg') : get_UrlBase('img/icono-ingresar.svg'); ?>" 
+                     alt="Ingresar" 
+                     width="30px">Ingresar</a> </li>
+        <li>
+            <a href="?opcion=Modificar" class="<?php echo isActive('Modificar', $opcion); ?>">
+                <img class="menu-icon" 
+                     data-default-src="<?php echo get_UrlBase('img/icono-modificar.svg'); ?>" 
+                     data-alt-src="<?php echo get_UrlBase('img/icono-modificar-relleno.svg'); ?>" 
+                     src="<?php echo isActive('Modificar', $opcion) ? get_UrlBase('img/icono-modificar-relleno.svg') : get_UrlBase('img/icono-modificar.svg'); ?>" 
+                     alt="Modificar" 
+                     width="30px">Modificar </a></li>
+        <li>
+            <a href="?opcion=Eliminar" class="<?php echo isActive('Eliminar', $opcion); ?>">
+                <img class="menu-icon" 
+                     data-default-src="<?php echo get_UrlBase('img/icono-eliminar.svg'); ?>" 
+                     data-alt-src="<?php echo get_UrlBase('img/icono-eliminar-relleno.svg'); ?>" 
+                     src="<?php echo isActive('Eliminar', $opcion) ? get_UrlBase('img/icono-eliminar-relleno.svg') : get_UrlBase('img/icono-eliminar.svg'); ?>" 
+                     alt="Eliminar" 
+                     width="30px">Eliminar</a></li>
+        <li> <a class="salir" href="<?php echo get_controllers('logout.php')?>"> <img src="<?php echo get_UrlBase('img/icono-salir.svg')?>" alt="Salir" width="30px">Salir</a> </li>
+    </ul>
+</div>
+
+
     <div class="contenido">
         <?php 
             echo $argumento; 
@@ -36,7 +75,8 @@ function mostrarDashboard($argumento) {
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="<?php echo get_UrlBase("js/modelo-carga.js")?>"></script>
+    <script src="<?php echo get_js("modelo-carga.js")?>"></script>
+    <script src="<?php echo get_js("modelo-cambiar-svg.js")?>"></script>
 </body>
 </html>
 
