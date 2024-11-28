445db5ee1ed86ebab4aacd213816d6dd2c1a0c7f
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-11-28 17:39:42 -0500

    Vista de eliminar usuarios

--- views/vistaEliminarUsuario.php
+++ views/vistaEliminarUsuario.php
@@ -43,7 +43,7 @@ function mostrarFormularioEliminar($mensaje = '', $usuario = null) {
     <link rel="stylesheet" href="<?php echo get_UrlBase('css/style-eliminarUsuario.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_UrlBase('css/style-mensaje-emergente.css'); ?>">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script> 
-    <script src="<?php echo get_UrlBase('js/modelo-mensaje-emergente.js'); ?>"></script>
+    <script src="<?php echo get_js('modelo-mensaje-emergente.js'); ?>"></script>
 <?php
 }
 ?>
