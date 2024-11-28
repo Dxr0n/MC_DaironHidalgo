375d0c23fd364e2a234827c4371d174c0de93adb
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-11-28 17:41:51 -0500

    Script para cambiar imagenes

--- js/modelo-cambiar-svg.js
+++ js/modelo-cambiar-svg.js
@@ -1,26 +1,19 @@
 $(document).ready(function () {
-    let activeLink = null; // Almacena el enlace actualmente activo
-
-    // Asocia el evento de clic a cada enlace
+    let activeLink = null; 
     $('.menu a').on('click', function (e) {
-        e.preventDefault(); // Evita la navegación automática
+        e.preventDefault();
+        const clickedLink = $(this); 
+        const icon = clickedLink.find('.menu-icon'); 
 
-        const clickedLink = $(this); // Enlace clickeado
-        const icon = clickedLink.find('.menu-icon'); // Encuentra el ícono dentro del enlace
-
-        // Restablece todos los íconos a su estado inicial
         $('.menu-icon').each(function () {
             const resetIcon = $(this);
             resetIcon.attr('src', resetIcon.data('default-src'));
         });
 
-        // Cambia la imagen del ícono del enlace clickeado
         icon.attr('src', icon.data('alt-src'));
 
-        // Actualiza el enlace activo
         activeLink = clickedLink;
 
-        // Redirige manualmente a la URL del enlace clickeado
         window.location.href = clickedLink.attr('href');
     });
 });
