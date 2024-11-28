613b8906532fd71ff3fe3b5bcc5e7d1a1d53e23b
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-11-27 21:00:34 -0500

    Controlador del login

---
+++ controllers/controladorLogin.php
@@ -0,0 +1,30 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/connect/conexion.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/modeloUsuario.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/views/vistaLogin.php';
+
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $v_username = $_POST["txtusername"] ?? '';
+        $v_password = $_POST["txtpassword"] ?? '';
+
+        $modelousuario = new modelousuario();
+        $user = $modelousuario->ingresarLogin($v_username);
+
+        if ($user && $v_password === $user['password']) {
+            $_SESSION["txtusername"] = $v_username;
+            header('Location: ' . get_controllers('controladorDashboard.php'));
+            exit;
+        } else {
+            header('Location: ' . get_views('claveequivocada.php'));
+            exit;
+        }
+} else {
+    mostrarLogin();
+}
+
+?>
