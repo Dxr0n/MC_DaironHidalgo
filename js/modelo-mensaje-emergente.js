824658a4a5645e01a7075c7865b3e676a85d7bb3
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-12-04 21:02:06 -0500

    Mensaje Emergente

--- js/modelo-mensaje-emergente.js
+++ js/modelo-mensaje-emergente.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-    const mensajeEmergente = $('#mensaje-emergente');
-    if (mensajeEmergente.length) { 
+document.addEventListener('DOMContentLoaded', function() {
+    const mensajeEmergente = document.getElementById('mensaje-emergente');
+    
+    if (mensajeEmergente) { 
         setTimeout(function() {
-            mensajeEmergente.fadeOut(1000, function() { 
-                $(this).remove(); 
-            });
-        }, 2500); 
+            mensajeEmergente.style.transition = 'opacity 1s';
+            mensajeEmergente.style.opacity = 0;
+
+            setTimeout(function() {
+                mensajeEmergente.remove();
+            }, 1000);  
+        }, 2500);  
     }
 });
