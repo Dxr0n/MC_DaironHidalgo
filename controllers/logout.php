05e707944165ff58b3b7c4faefee8a1a208def10
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-12-04 20:54:32 -0500

    Controlador del Salir

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,10 +1,13 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/etc/config.php';
-    session_start();
+    
+    if (session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
 
     session_unset();
     
     session_destroy();
     header("Location: ".get_UrlBase('index.php'));
-    exit;
+    exit();
 ?>
