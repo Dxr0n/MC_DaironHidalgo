88bcbd13c3f208b47bda7ec4c0dabb98020ce224
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-12-04 21:01:42 -0500

    Carga

--- js/modelo-carga.js
+++ js/modelo-carga.js
@@ -1,6 +1,12 @@
-window.onload = function(){
-    $('#carga').fadeOut(2000);
-   setTimeout(function() {
-    document.getElementById("#carga").style.display = "none"; 
-}, 6000); 
+window.onload = function() {
+    var cargaElement = document.getElementById('carga');
+    
+    if (cargaElement) {
+        cargaElement.style.transition = 'opacity 2s';
+        cargaElement.style.opacity = 0;
+
+        setTimeout(function() {
+            cargaElement.style.display = 'none';
+        }, 2200);  
+    }
 }
