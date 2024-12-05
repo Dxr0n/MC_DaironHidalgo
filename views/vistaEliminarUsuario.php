049e6a7387ccc2f8c7fef06265ee4b4d1c45074d
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2024-12-04 21:05:33 -0500

    Vista Eliminar

--- views/vistaEliminarUsuario.php
+++ views/vistaEliminarUsuario.php
@@ -5,7 +5,7 @@ function mostrarFormularioEliminar($mensaje = '', $usuario = null) {
         <h2>Eliminar Usuario</h2>
 
         <?php if (!$usuario) { ?>
-            <form action="" method="POST" autocomplete="off">
+            <form action="<?php get_controllers('controladorIngresarUsuario.php'); ?>" method="POST" autocomplete="off">
                 <label for="delete_id">ID del usuario a eliminar:</label>
                 <input type="number" id="delete_id" name="delete_id" min="1" required>
                 <br>
@@ -26,7 +26,7 @@ function mostrarFormularioEliminar($mensaje = '', $usuario = null) {
                         <td><?php echo $usuario['perfil']; ?></td>
                     </tr>
                 </table>
-                <form action="" method="POST">
+                <form action="<?php get_controllers('controladorIngresarUsuario.php'); ?>" method="POST">
                     <input type="hidden" name="delete_id" value="<?php echo $usuario['id']; ?>">
                     <button type="submit" name="confirmar_eliminacion">Confirmar Eliminación</button>
                 </form>
